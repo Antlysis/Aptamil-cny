5b337a260f65c041ba2db049995d46ebfabae5c2
Xin Yi <xinyi@thedgroup.com.my>
2024-12-19 15:56:41 +0800

    allow closing outside modal

--- src/components/RewardModal/index.tsx
+++ src/components/RewardModal/index.tsx
@@ -32,7 +32,7 @@ function RewardModal({ reward, isOpen, onClose }: RewardModalProps) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 pb-[73px]">
-      <div className="fixed inset-0 bg-black bg-opacity-50"></div>
+      <div className="fixed inset-0 bg-black bg-opacity-50" onClick={onClose}></div>
       <div className="relative w-full max-w-lg">
         <img
           src={getRewardImage()}
