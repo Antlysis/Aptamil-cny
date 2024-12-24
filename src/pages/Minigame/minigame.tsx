d70a88b9d479742e68a799f6f64a38b4424c6e64
LiChuanXD <billgatepls@hotmail.com>
2024-12-24 08:55:54 +0800

    feature: added fallback event listener for game complete

--- src/pages/Minigame/minigame.tsx
+++ src/pages/Minigame/minigame.tsx
@@ -21,68 +21,116 @@ const MiniGame: React.FC = () => {
   const aptamilCampaign = import.meta.env.VITE_APP_APTAMIL;
 
   useEffect(() => {
+    let processing = false;
+
     const handleGameMessage = async (event: MessageEvent) => {
-      if (event.data.action === 'gameCompleted') {
-        try {
-          const existingMinigame =
-            userDetails?.data?.personalInfo?.extendedFields?.minigame || [];
+      if (!processing) {
+        processing = true;
+        if (event.data.action === 'gameCompleted') {
+          try {
+            const existingMinigame =
+              userDetails?.data?.personalInfo?.extendedFields?.minigame || [];
 
-          const updatedMinigame = [
-            ...existingMinigame,
-            {
-              date: new Date().toLocaleString('en-GB'),
-              timer: event.data.data,
-            },
-          ];
-
-          const updateData = {
-            values: {
-              extendedFields: {
-                minigame: updatedMinigame,
+            const updatedMinigame = [
+              ...existingMinigame,
+              {
+                date: new Date().toLocaleString('en-GB'),
+                timer: event.data.data,
               },
-            },
-          };
-          console.log('Update Payload:', JSON.stringify(updateData, null, 2));
-          console.log('updated');
-          await updateProfileOutput(updateData);
-          navigate(`/minigame/result?time=${event.data.data}`);
-          const res = await getUserDetailsAPI();
-          if (res) {
-            dispatch(setUserDetails(res?.data));
+            ];
+
+            const updateData = {
+              values: {
+                extendedFields: {
+                  minigame: updatedMinigame,
+                },
+              },
+            };
+            console.log('Update Payload:', JSON.stringify(updateData, null, 2));
+            console.log('updated');
+            await updateProfileOutput(updateData);
+            const res = await getUserDetailsAPI();
+            if (res) {
+              dispatch(setUserDetails(res?.data));
+            }
+            navigate(`/minigame/result?time=${event.data.data}`);
+          } catch (error) {
+            navigate(`/minigame/result?time=${event.data.data}`);
           }
-        } catch (error) {
-          alert('Failed to update profile');
+        }
+      }
+    };
+
+    // fallback function if window message event is not triggered
+    const storageEventListener = async (event: StorageEvent) => {
+      if (!processing) {
+        processing = true;
+        if (event.key === 'gameCompleted') {
+          const timer = parseInt(event.newValue || '0');
+          try {
+            const existingMinigame =
+              userDetails?.data?.personalInfo?.extendedFields?.minigame || [];
+
+            const updatedMinigame = [
+              ...existingMinigame,
+              {
+                date: new Date().toLocaleString('en-GB'),
+                timer,
+              },
+            ];
+
+            const updateData = {
+              values: {
+                extendedFields: {
+                  minigame: updatedMinigame,
+                },
+              },
+            };
+
+            await updateProfileOutput(updateData);
+            const res = await getUserDetailsAPI();
+            if (res) {
+              dispatch(setUserDetails(res?.data));
+            }
+            navigate(`/minigame/result?time=${timer}`);
+          } catch (error) {
+            navigate(`/minigame/result?time=${timer}`);
+          }
         }
       }
     };
 
     window.addEventListener('message', handleGameMessage);
-    return () => window.removeEventListener('message', handleGameMessage);
+    window.addEventListener('storage', storageEventListener);
+    return () => {
+      window.removeEventListener('message', handleGameMessage);
+      window.removeEventListener('storage', storageEventListener);
+    };
   }, [navigate]);
 
   return (
     <div id="page" className="overflow-y-auto">
-      <div className="absolute flex justify-between w-full">
+      <div className="absolute flex w-full justify-between">
         <Header previous={true} />
       </div>
-      <div className="relative overflow-hidden z-[3] pb-[73px]">
+      <div className="relative z-[3] overflow-hidden pb-[73px]">
         <img
           src={gameBackground}
           alt="main-bg"
-          className="absolute w-full min-h-screen top-0 left-0"
+          className="absolute left-0 top-0 min-h-screen w-full"
         />
-        <div className="pt-[100px] w-[90%] mx-auto flex flex-col relative z-[2] ">
+        <div className="relative z-[2] mx-auto flex w-[90%] flex-col pt-[100px] ">
           <iframe
-            src="../../../Card Flipping Game/index.html"
-            // src="/game/index.html"
+            // src="../../../contest/Card Flipping Game/index.html"
+            src="./game/index.html"
             title="Game"
-            className="w-full h-[402px]"
+            className="h-[402px] w-full"
           ></iframe>
           <img src={howTo} alt="How To" className="pt-5" />
         </div>
         <HotLineButton top="top-1/2" />
         <div className="footer-div">
-          <div className="relative z-40 w-full mx-auto text-center my-3">
+          <div className="relative z-40 mx-auto my-3 w-full text-center">
             <ButtonComponent
               buttonText="UPLOAD RECEIPT"
               buttonType="button"
@@ -90,9 +138,9 @@ const MiniGame: React.FC = () => {
               navigateTo="/upload"
             />
           </div>
-          <div className="grid flex-grid grid-cols-2 gap-2 w-[90%] mx-auto">
+          <div className="flex-grid mx-auto grid w-[90%] grid-cols-2 gap-2">
             <div className="relative">
-              <div className="z-40 w-full mx-auto text-center top-[140px]">
+              <div className="top-[140px] z-40 mx-auto w-full text-center">
                 <ButtonComponent
                   buttonText="TRACK MY SUBMISSION"
                   buttonType="button"
@@ -104,12 +152,12 @@ const MiniGame: React.FC = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <p className="text-white underline mt-2">Terms & Conditions</p>
+                  <p className="mt-2 text-white underline">Terms & Conditions</p>
                 </a>
               </div>
             </div>
             <div className="relative">
-              <div className="z-40 w-full mx-auto text-center top-[140px]">
+              <div className="top-[140px] z-40 mx-auto w-full text-center">
                 <ButtonComponent
                   buttonText="HOW TO JOIN"
                   buttonType="button"
@@ -124,7 +172,7 @@ const MiniGame: React.FC = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <p className="text-white underline mt-2">Privacy Policy</p>
+                  <p className="mt-2 text-white underline">Privacy Policy</p>
                 </a>
               </div>
             </div>
